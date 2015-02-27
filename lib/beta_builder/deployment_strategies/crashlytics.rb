4e3c9d7c75aa780e1fc18d7116e80b7d95876f5a
Stas Nikiforov <stas@airtime.com>
2015-02-26 19:16:10 -0500

    Add support for deploying with Crashlytics

---
+++ lib/beta_builder/deployment_strategies/crashlytics.rb
@@ -0,0 +1,42 @@
+require 'rest_client'
+require 'json'
+require 'tmpdir'
+require 'fileutils'
+
+module BetaBuilder
+  module DeploymentStrategies
+    class Crashlytics < Strategy
+      def extended_configuration_for_strategy
+        proc do
+          def generate_release_notes(&block)
+            self.release_notes = block if block
+          end
+        end
+      end
+
+      def deploy
+        release_notes = @configuration.release_notes.call
+        puts "Release notes are #{release_notes}"
+        puts "ipa_path: #{@configuration.ipa_path}"
+        dir = Dir.mktmpdir
+        begin
+          filepath = "#{dir}/release_notes.txt"
+          res = IO.write(filepath, release_notes)
+          unless res
+            puts "Failed to create temp release notes file"
+          else
+            puts "Distributing build on Crashlytics..."
+            res = system("bubonic/ext/Crashlytics.framework/submit #{@configuration.api_key} #{@configuration.build_secret} -ipaPath #{@configuration.ipa_path} -emails stas@airtime.com -notesPath #{filepath}")
+            if res
+              puts "Upload complete."
+            else
+              puts "Upload failed: #{res}"
+            end
+          end
+        ensure
+          FileUtils.rm_rf(dir)
+        end
+      end
+    end
+  end
+end
